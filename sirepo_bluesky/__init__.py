f8f022bb4c54193f1643263a78f4dd70c4e8aa5a
AbbyGi <abbygi09@gmail.com>
2020-02-27 16:49:23 -0500

    Added RunEngine, DataBroker, and ROOT_DIR for now

--- sirepo_bluesky/__init__.py
+++ sirepo_bluesky/__init__.py
@@ -1,4 +1,38 @@
+import datetime
+
+from bluesky.run_engine import RunEngine
+from bluesky.callbacks import best_effort
+from bluesky.utils import install_kicker
+
+import databroker
+from databroker import Broker
+
+from ophyd.utils import make_dir_tree
+
+from sirepo_bluesky.srw_handler import SRWFileHandler
+import matplotlib.pyplot as plt
 
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+RE = RunEngine({})
+bec = best_effort.BestEffortCallback()
+RE.subscribe(bec)
+
+# MongoDB backend:
+db = Broker.named('local')  # mongodb backend
+try:
+    databroker.assets.utils.install_sentinels(db.reg.config, version=1)
+except:
+    pass
+
+RE.subscribe(db.insert)
+db.reg.register_handler('srw', SRWFileHandler, overwrite=True)
+db.reg.register_handler('SIREPO_FLYER', SRWFileHandler, overwrite=True)
+
+plt.ion()
+install_kicker()
+
+ROOT_DIR = '/tmp/sirepo_flyer_data'
+_ = make_dir_tree(datetime.datetime.now().year, base_path=ROOT_DIR)
