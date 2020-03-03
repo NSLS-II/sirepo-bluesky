fed8c6ec983ee620590091e35d0a902c89ffd567
Maksim Rakitin <mrakitin@bnl.gov>
2020-03-03 09:58:26 -0500

    Use DRY principle in tests

--- sirepo_bluesky/tests/test_sirepo_flyer.py
+++ sirepo_bluesky/tests/test_sirepo_flyer.py
@@ -27,8 +27,7 @@ def test_smoke_sirepo_docker():
     assert 'beamline' in data['models']
 
 
-@vcr.use_cassette(f'{cassette_location}/test_sirepo_flyer.yml')
-def test_sirepo_flyer(RE_no_plot, db, tmpdir):
+def _test_sirepo_flyer(RE_no_plot, db, tmpdir, sim_id, server_name):
     import datetime
     from ophyd.utils import make_dir_tree
 
@@ -49,7 +48,7 @@ def test_sirepo_flyer(RE_no_plot, db, tmpdir):
                                  key2: parameters_update2,
                                  key3: parameters_update3})
 
-    sirepo_flyer = SirepoFlyer(sim_id='87XJ4oEb', server_name='http://10.10.10.10:8000',
+    sirepo_flyer = SirepoFlyer(sim_id=sim_id, server_name=server_name,
                                root_dir=root_dir, params_to_change=params_to_change,
                                watch_name='W60', run_parallel=False)
 
@@ -67,45 +66,17 @@ def test_sirepo_flyer(RE_no_plot, db, tmpdir):
         db_means.append(t.iloc[i]['sirepo_flyer_mean'])
 
     assert actual_means == db_means, "fly scan means do not match actual means"
+
+
+@vcr.use_cassette(f'{cassette_location}/test_sirepo_flyer.yml')
+def test_sirepo_flyer_vcr(RE_no_plot, db, tmpdir):
+    _test_sirepo_flyer(RE_no_plot, db, tmpdir,
+                       sim_id='87XJ4oEb',
+                       server_name='http://10.10.10.10:8000')
 
 
 @pytest.mark.docker
 def test_sirepo_flyer_docker(RE_no_plot, db, tmpdir):
-    import datetime
-    from ophyd.utils import make_dir_tree
-
-    RE_no_plot.subscribe(db.insert)
-
-    root_dir = f'{tmpdir}/sirepo_flyer_data'
-    _ = make_dir_tree(datetime.datetime.now().year, base_path=root_dir)
-
-    params_to_change = []
-    for i in range(1, 6):
-        key1 = 'Aperture'
-        parameters_update1 = {'horizontalSize': i * .1, 'verticalSize': (16 - i) * .1}
-        key2 = 'Lens'
-        parameters_update2 = {'horizontalFocalLength': i + 7}
-        key3 = 'Obstacle'
-        parameters_update3 = {'horizontalSize': 6 - i}
-        params_to_change.append({key1: parameters_update1,
-                                 key2: parameters_update2,
-                                 key3: parameters_update3})
-
-    sirepo_flyer = SirepoFlyer(sim_id='00000000', server_name='http://localhost:8000',
-                               root_dir=root_dir, params_to_change=params_to_change,
-                               watch_name='W60', run_parallel=False)
-
-    RE_no_plot(bp.fly([sirepo_flyer]))
-
-    hdr = db[-1]
-    t = hdr.table(stream_name='sirepo_flyer')
-    db_means = []
-    actual_means = [36779651609602.38,
-                    99449330615601.89,
-                    149289119385413.34,
-                    223428480785808.78,
-                    388594677365777.9]
-    for i in range(len(t)):
-        db_means.append(t.iloc[i]['sirepo_flyer_mean'])
-
-    assert actual_means == db_means, "fly scan means do not match actual means"
+    _test_sirepo_flyer(RE_no_plot, db, tmpdir,
+                       sim_id='00000000',
+                       server_name='http://localhost:8000')
