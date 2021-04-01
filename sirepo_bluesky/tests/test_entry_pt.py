feae77b7a7a8087aa51b9998d25fd24fe6f9ebcc
vagrant <vagrant@vagrant.vm>
2021-04-01 18:41:11 +0000

    Fix test

--- sirepo_bluesky/tests/test_entry_pt.py
+++ sirepo_bluesky/tests/test_entry_pt.py
@@ -1,15 +1,5 @@
 from databroker import Broker
-#from bluesky import RunEngine
-#from bluesky.callbacks.best_effort import BestEffortCallback
 
-
-#RE = RunEngine()
-#bec = BestEffortCallback()
-#RE.subscribe(bec)
-#db = Broker.named('bluesky-cartpole')
-#db = Broker.named('sirepo-bluesky')
 def test_broker():
 	db = Broker.named("local")
-
-#RE.subscribe(db.insert)
 
