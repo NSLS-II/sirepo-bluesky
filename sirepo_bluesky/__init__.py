450ae598c7f8645543b26cfcf3e68a450c6cb71e
Maksim Rakitin <mrakitin@users.noreply.github.com>
2021-04-21 19:40:36 -0400

    Fix the mongodb host name

--- sirepo_bluesky/__init__.py
+++ sirepo_bluesky/__init__.py
@@ -8,5 +8,5 @@ catalog_class = intake.registry["bluesky-mongo-normalized-catalog"]
 
 sirepo_bluesky_catalog_instance = catalog_class(
         metadatastore_db="mongodb://localhost:27017/md",
-        asset_registry_db="mongodb://localhosty:27017/ar",
+        asset_registry_db="mongodb://localhost:27017/ar",
 )
