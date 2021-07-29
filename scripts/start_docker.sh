6407b362d675d88c47dcdcd03dc43141445c6a8a
Maksim Rakitin <mrakitin@bnl.gov>
2021-07-29 14:55:55 -0400

    Update the script to pull docker image before running it

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -6,6 +6,8 @@ year=$(date +"%Y")
 
 today="${HOME}/tmp/data/${year}/${month}/${day}"
 
+docker_image="radiasoft/sirepo:beta"
+
 if [ -d "${today}" ]
 then
     echo "Directory ${today} exists."
@@ -16,6 +18,10 @@ fi
 
 ls -l $HOME/tmp
 
+docker pull ${docker_image}
+
+docker images
+
 # specify -it or -d on the command line
 docker run $1 --init --rm --name sirepo \
        -e SIREPO_AUTH_METHODS=bluesky:guest \
@@ -24,4 +30,4 @@ docker run $1 --init --rm --name sirepo \
        -e SIREPO_COOKIE_IS_SECURE=false \
        -p 8000:8000 \
        -v $PWD/sirepo_bluesky/tests/SIREPO_SRDB_ROOT:/SIREPO_SRDB_ROOT:ro,z \
-       radiasoft/sirepo:beta bash -l -c "mkdir -v -p /sirepo/ && cp -Rv /SIREPO_SRDB_ROOT/* /sirepo/ && sirepo service http"
+       ${docker_image} bash -l -c "mkdir -v -p /sirepo/ && cp -Rv /SIREPO_SRDB_ROOT/* /sirepo/ && sirepo service http"
