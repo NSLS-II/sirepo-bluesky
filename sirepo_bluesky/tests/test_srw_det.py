06bdf6c7f2a412cbaa2b2a920591c4e8bb6f8031
Maksim Rakitin <mrakitin@bnl.gov>
2021-07-29 14:52:59 -0400

    TST: fix tests & rename sirepo_det -> srw_det there

--- sirepo_bluesky/tests/test_srw_det.py
+++ sirepo_bluesky/tests/test_srw_det.py
@@ -19,24 +19,25 @@ def _test_srw_detector(RE, db, tmpdir, sim_type, sim_id, server_name, sim_report
     root_dir = '/tmp/data'
     _ = make_dir_tree(datetime.datetime.now().year, base_path=root_dir)
 
-    sirepo_det = SirepoSRWDetector(sim_type=sim_type, sim_report_type=sim_report_type, sim_id=sim_id,
+    srw_det = SirepoSRWDetector(name="srw_det", sim_type=sim_type,
+                                sim_report_type=sim_report_type, sim_id=sim_id,
                                 sirepo_server=server_name, root_dir=root_dir)
-    sirepo_det.select_optic('Aperture')
-    sirepo_det.create_parameter('horizontalSize')
-    sirepo_det.create_parameter('verticalSize')
-    sirepo_det.read_attrs = ['image', 'mean', 'photon_energy']
-    sirepo_det.configuration_attrs = ['horizontal_extent',
+    srw_det.select_optic('Aperture')
+    srw_det.create_parameter('horizontalSize')
+    srw_det.create_parameter('verticalSize')
+    srw_det.read_attrs = ['image', 'mean', 'photon_energy']
+    srw_det.configuration_attrs = ['horizontal_extent',
                                       'vertical_extent',
                                       'shape']
 
-    sirepo_det.active_parameters['Aperture_horizontalSize'].set(1.0)
-    sirepo_det.active_parameters['Aperture_verticalSize'].set(1.0)
+    srw_det.active_parameters['Aperture_horizontalSize'].set(1.0)
+    srw_det.active_parameters['Aperture_verticalSize'].set(1.0)
 
-    RE(bp.count([sirepo_det]))
+    RE(bp.count([srw_det]))
 
     hdr = db[-1]
     t = hdr.table()
-    mean = t.iloc[0]['sirepo_det_mean']
+    mean = t.iloc[0]['srw_det_mean']
 
     assert mean == 1334615738479247.2, "incorrect mean value from bp.count"
 
@@ -59,7 +60,8 @@ def test_srw_detector_docker(RE, db, tmpdir):
                           server_name='http://localhost:8000')
 
 
-def _test_srw_det_grid_scan(RE, db, tmpdir, sim_type, sim_id, server_name, sim_report_type="srw_se_spectrum"):
+def _test_srw_det_grid_scan(RE, db, tmpdir, sim_type, sim_id, server_name,
+                            sim_report_type="srw_se_spectrum"):
     import datetime
     from ophyd.utils import make_dir_tree
 
@@ -68,17 +70,18 @@ def _test_srw_det_grid_scan(RE, db, tmpdir, sim_type, sim_id, server_name, sim_r
     root_dir = '/tmp/data'
     _ = make_dir_tree(datetime.datetime.now().year, base_path=root_dir)
 
-    sirepo_det = SirepoSRWDetector(sim_type=sim_type, sim_report_type=sim_report_type, sim_id=sim_id,
+    srw_det = SirepoSRWDetector(name="srw_det", sim_type=sim_type,
+                                sim_report_type=sim_report_type, sim_id=sim_id,
                                 sirepo_server=server_name, root_dir=root_dir)
-    sirepo_det.select_optic('Aperture')
-    param1 = sirepo_det.create_parameter('horizontalSize')
-    param2 = sirepo_det.create_parameter('verticalSize')
-    sirepo_det.read_attrs = ['image', 'mean', 'photon_energy']
-    sirepo_det.configuration_attrs = ['horizontal_extent',
-                                      'vertical_extent',
-                                      'shape']
+    srw_det.select_optic('Aperture')
+    param1 = srw_det.create_parameter('horizontalSize')
+    param2 = srw_det.create_parameter('verticalSize')
+    srw_det.read_attrs = ['image', 'mean', 'photon_energy']
+    srw_det.configuration_attrs = ['horizontal_extent',
+                                   'vertical_extent',
+                                   'shape']
 
-    RE(bp.grid_scan([sirepo_det],
+    RE(bp.grid_scan([srw_det],
                     param1, 0, 1, 3,
                     param2, 0, 1, 3,
                     True))
@@ -97,13 +100,13 @@ def _test_srw_det_grid_scan(RE, db, tmpdir, sim_type, sim_id, server_name, sim_r
     hdr = db[-1]
     t = hdr.table()
     for i in range(len(t)):
-        db_means.append(t.iloc[i]['sirepo_det_mean'])
+        db_means.append(t.iloc[i]['srw_det_mean'])
     assert actual_means == db_means, "grid_scan means do not match actual means"
 
 
 @vcr.use_cassette(f'{cassette_location}/test_srw_det_grid_scan.yml')
 def test_srw_det_grid_scan_vcr(RE, db, tmpdir):
-    _test_sirepo_det_grid_scan(RE, db, tmpdir,
+    _test_srw_det_grid_scan(RE, db, tmpdir,
                                sim_type='srw',
                                sim_report_type="srw_se_spectrum",
                                sim_id='e75qHII6',
@@ -112,7 +115,7 @@ def test_srw_det_grid_scan_vcr(RE, db, tmpdir):
 
 @pytest.mark.docker
 def test_srw_det_grid_scan_docker(RE, db, tmpdir):
-    _test_sirepo_det_grid_scan(RE, db, tmpdir,
+    _test_srw_det_grid_scan(RE, db, tmpdir,
                                sim_type='srw',
                                sim_report_type="srw_se_spectrum",
                                sim_id='00000001',
