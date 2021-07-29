5d1f34579a94e2f248635f4d6e56cd695e934340
Maksim Rakitin <mrakitin@bnl.gov>
2021-07-29 15:42:10 -0400

    Make the photon_energy component "normal"

--- sirepo_bluesky/srw_detector.py
+++ sirepo_bluesky/srw_detector.py
@@ -58,7 +58,7 @@ class SirepoSRWDetector(Device):
     image = Cpt(ExternalFileReference, kind="normal")
     shape = Cpt(Signal)
     mean = Cpt(Signal, kind="hinted")
-    photon_energy = Cpt(Signal, kind="hinted")
+    photon_energy = Cpt(Signal, kind="normal")
     horizontal_extent = Cpt(Signal)
     vertical_extent = Cpt(Signal)
     sirepo_json = Cpt(Signal, kind="normal", value="")
