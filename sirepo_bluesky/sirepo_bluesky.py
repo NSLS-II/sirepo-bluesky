8463d592af99fdfa89337aab9c669549cd860899
Abigail Giles <agiles@bnl.gov>
2022-03-10 14:12:55 -0500

    Parametrize file index

--- sirepo_bluesky/sirepo_bluesky.py
+++ sirepo_bluesky/sirepo_bluesky.py
@@ -121,7 +121,7 @@ class SirepoBluesky(object):
                 return optic_id
         raise ValueError(f'Not valid optic {optic_name}')
 
-    def get_datafile(self):
+    def get_datafile(self, file_index=-1):
         """ Request the raw datafile of simulation results from the server.
 
             Notes
@@ -129,10 +129,7 @@ class SirepoBluesky(object):
             Call auth() and run_simulation() before this.
         """
         assert hasattr(self, 'cookies'), 'call auth() before get_datafile()'
-        if self.sim_type in ["srw", "shadow"]:
-            url = 'download-data-file/{}/{}/{}/-1'.format(self.sim_type, self.sim_id, self.data['report'])
-        elif self.sim_type in ["madx"]:
-            url = 'download-data-file/{}/{}/{}/0'.format(self.sim_type, self.sim_id, self.data['report'])
+        url = f"download-data-file/{self.sim_type}/{self.sim_id}/{self.data['report']}/{file_index}"
         response = requests.get('{}/{}'.format(self.server, url), cookies=self.cookies)
         self._assert_success(response, url)
         return response.content
