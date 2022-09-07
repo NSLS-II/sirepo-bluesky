591ca8b7ed284c7832cd70a1d3d3671ebe6a95b7
Maksim Rakitin <mrakitin@bnl.gov>
2022-09-06 20:23:00 -0400

    STY: flake8

--- sirepo_bluesky/madx_handler.py
+++ sirepo_bluesky/madx_handler.py
@@ -1,4 +1,3 @@
-import numpy as np
 import tfs
 from area_detector_handlers import HandlerBase
 
@@ -16,39 +15,3 @@ class MADXFileHandler(HandlerBase):
 
     def __call__(self, row_num=0, col_name="NAME"):
         return self._dataframe[col_name][row_num]
-
-
-# vvv Below is srw handler code vvv
-# ---------------------------------
-# def read_srw_file(filename):
-#     data, mode, ranges, labels, units = srw_io.file_load(filename)
-#     data = np.array(data)
-#     if ndim == 2:
-#         data = data.reshape((ranges[8], ranges[5]), order='C')
-#         photon_energy = ranges[0]
-#     elif ndim == 1:
-#         photon_energy = np.linspace(*ranges[:3])
-#     else:
-#         raise ValueError(f'The value ndim={ndim} is not supported.')
-
-#     return {'data': data,
-#             'shape': data.shape,
-#             'mean': np.mean(data),
-#             'photon_energy': photon_energy,
-#             'horizontal_extent': ranges[3:5],
-#             'vertical_extent': ranges[6:8],
-#             # 'mode': mode,
-#             'labels': labels,
-#             'units': units}
-
-
-# class SRWFileHandler:
-#     specs = {'srw'}
-
-#     def __init__(self, filename, ndim=2):
-#         self._name = filename
-#         self._ndim = ndim
-
-#     def __call__(self):
-#         d = read_srw_file(self._name, ndim=self._ndim)
-#         return d['data']
