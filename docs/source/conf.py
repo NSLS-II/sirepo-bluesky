97d7108f7e67290076d5b154ebf1659d6fdb9ea9
Maksim Rakitin <mrakitin@bnl.gov>
2022-09-19 17:57:10 -0400

    STY: move import of sptheme to top of sphinx `conf.py`

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import cloud_sptheme as sptheme
 
 # -- General configuration ------------------------------------------------
 
@@ -107,8 +108,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 html_theme = 'cloud'
-import cloud_sptheme
-html_theme_path = [cloud_sptheme.get_theme_dir()]
+html_theme_path = [sptheme.get_theme_dir()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
