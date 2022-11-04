ca5c455e3bc0d024a501c8b546f407d0868014d5
Maksim Rakitin <mrakitin@bnl.gov>
2022-11-04 13:43:46 -0400

    CI/BLD: minimum supported version is Python 3.8 now

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import versioneer
 # NOTE: This file must remain Python 2 compatible for the foreseeable future,
 # to ensure that we error out properly for people with outdated setuptools
 # and/or pip.
-min_version = (3, 6)
+min_version = (3, 8)
 if sys.version_info < min_version:
     error = """
 sirepo-bluesky does not support Python {0}.{1}.
@@ -40,7 +40,7 @@ setup(
     description="Sirepo-Bluesky interface",
     long_description=readme,
     author="Brookhaven National Laboratory",
-    author_email='DAMA@bnl.gov',
+    author_email='mrakitin@bnl.gov',
     url='https://github.com/NSLS-II/sirepo-bluesky',
     python_requires='>={}'.format('.'.join(str(n) for n in min_version)),
     packages=find_packages(exclude=['docs', 'tests']),
